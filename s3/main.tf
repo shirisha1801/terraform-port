c7952e1a87d27288dc9b17f678bd5be1c9ec4957
shirisha1801 <104202313+shirisha1801@users.noreply.github.com>
2026-08-30 17:03:02 -0500

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,1 +1,28 @@
+terraform {
+  cloud {
+    organization = "shirisha-lab"
 
+    workspaces {
+      name = "port-lab"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.bucket.id
+}
